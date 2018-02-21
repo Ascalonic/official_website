497d04b5bb0f6562a563f64c495490d93c0fc871
Varghese Mathai <varghesemathaime@gmail.com>
2018-02-21 21:31:07 +0530

    Vyapti basic

--- vyapti/js/index.js
+++ vyapti/js/index.js
@@ -14,7 +14,7 @@ $header_top.find('a').on('click', function() {
 
 // fullpage customization
 $('#fullpage').fullpage({
-  sectionsColor: ['#FEAC0A', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+  sectionsColor: ['#FFFFFF', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
   sectionSelector: '.vertical-scrolling',
   slideSelector: '.horizontal-scrolling',
   navigation: true,
@@ -24,7 +24,7 @@ $('#fullpage').fullpage({
   menu: '#menu',
 
   afterLoad: function(anchorLink, index) {
-    $header_top.css('background', 'rgba(0, 47, 77, .3)');
+    $header_top.css('background', 'rgb(0, 0, 0)');
     $nav.css('background', 'rgba(0, 47, 77, .25)');
     if (index == 5) {
         $('#fp-nav').hide();
@@ -231,3 +231,20 @@ $(document).ready(function() {
 		window.parallaxHover = new _ParallaxHover(this);
 	});
 });
+
+////////////////////////////////////////////////////////////////
+
+particlesJS("particles-js", {"particles":{"number":{"value":137,"density":{"enable":true,"value_area":789.1476416322727}},
+"color":{"value":"#1eff00"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},
+"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,
+"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,
+"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},
+"move":{"enable":true,"speed":3.1565905665290903,"direction":"top","random":false,"straight":false,"out_mode":"out","bounce":false,
+"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"repulse"},
+"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},
+"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
+var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; 
+stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); 
+
+update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) 
+{ count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
